7ce766ec219374198b479ccd280fe56151f406ac
chalusb <chaluuf@gamil.com>
2025-01-27 11:04:46 -0600

    sele agrega el script con lazy load

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: "Ford México | Nuevos Autos, SUVs, Pickups y Camiones",
@@ -21,6 +22,10 @@ export default function RootLayout({
           <Header />
           {children}
         </div>
+        <Script
+          src="https://assets.adobedtm.com/248d63c2ff6b/bec206858ddf/launch-64309b53b8ca.min.js"
+          strategy="lazyOnload"
+        /> 
         <Footer />
       </body>
     </html>
